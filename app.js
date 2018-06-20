36ba0cbae9efc7e24a1d5d292b95f3a8293234bb
DataNick <DataNick@users.noreply.github.com>
2018-06-19 21:57:41 -0400

    Uncommit app.use(express.static...

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 var path = require('path');
 
-// app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', function(req, res){
   // route to serve index html
